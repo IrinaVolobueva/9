2e606d36d8543df7034e4cbee502d7b4f167144f
IrinaVolobueva <145708238+IrinaVolobueva@users.noreply.github.com>
2024-07-02 21:31:34 +0300

    Add files via upload

---
+++ 9/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    public class ChangeCalculator
+    {
+        public Change CalculateChange(decimal cost, decimal money)
+        {
+            decimal changeAmount = money - cost;
+            Change change = new Change();
+
+            change.Rubles = (int)changeAmount;
+            decimal kopecks = (changeAmount - change.Rubles) * 100;
+
+            change.Kopecks = (int)kopecks;
+
+            int[] coinValues = { 50, 20, 10, 5, 2, 1 };
+            foreach (int coinValue in coinValues)
+            {
+                int count = (int)(kopecks / coinValue);
+                kopecks -= count * coinValue;
+                change.Coins.Add(new Coin(coinValue, count));
+            }
+
+            return change;
+        }
+    }
+
+    public class Change
+    {
+        public int Rubles { get; set; }
+        public int Kopecks { get; set; }
+        public List<Coin> Coins { get; set; } = new List<Coin>();
+    }
+
+    public class Coin
+    {
+        public int Value { get; set; }
+        public int Count { get; set; }
+
+        public Coin(int value, int count)
+        {
+            Value = value;
+            Count = count;
+        }
+    }
+}
---
+++ 9/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string costInput = textBox1.Text;
+            decimal cost;
+            string moneyInput = textBox2.Text;
+            decimal money;
+
+            if (!Decimal.TryParse(costInput, out cost) & !Decimal.TryParse(moneyInput, out money))
+            {
+                MessageBox.Show("Некорректный ввод. Пожалуйста, введите стоимость в формате 20.00.");
+            }
+            else
+            {
+                if (money < cost)
+                {
+                    MessageBox.Show("Количество денег недостаточно для оплаты товара.");
+                }
+
+                ChangeCalculator calculator = new ChangeCalculator();
+                Change change = calculator.CalculateChange(cost, money);
+
+                textBox3.Text = $"Рублей: {change.Rubles}, копеек: {change.Kopecks}";
+                foreach (Coin coin in change.Coins)
+                {
+                    MessageBox.Show($"Мелкая сдача: {coin.Value} копеек: {coin.Count} штук");
+                }
+            }
+        }
+    }
+}
